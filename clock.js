7f3e141dc51427feaf618a23fd1fa74c3ac3a5fc
Sushant Singh <sushantbibhu@gmail.com>
2023-06-23 18:54:47 +0530

    declare-variables

---
+++ clock.js
@@ -0,0 +1,6 @@
+function clock(){
+    let hours = document.getElementById('hours');
+    let minutes = document.getElementById('minutes');
+    let seconds = document.getElementById('seconds');
+    let period = document.getElementById('period');
+}
